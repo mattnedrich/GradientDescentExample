c51f486241e51e47f13d3e00d63f5e1bea4d5165
Matt Nedrich <matt.nedrich@atomicobject.com>
2015-03-09 23:14:32 -0400

    code clean up

--- gradient_descent_example.py
+++ gradient_descent_example.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # y = mx + b
 # m is slope, b is y-intercept
-def computer_error_for_line_given_points(b, m, points):
+def compute_error_for_line_given_points(b, m, points):
     totalError = 0
     for i in range(0, len(points)):
         x = points[i, 0]
@@ -37,13 +37,10 @@ def run():
     initial_b = 0 # initial y-intercept guess
     initial_m = 0 # initial slope guess
     num_iterations = 1000
-    initial_error = computer_error_for_line_given_points(initial_b, initial_m, points)
-    print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, initial_error)
+    print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points))
     print "Running..."
     [b, m] = gradient_descent_runner(points, initial_b, initial_m, num_iterations)
-    
-    after_error = computer_error_for_line_given_points(b, m, points)
-    print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, after_error)
+    print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
 
 if __name__ == '__main__':
     run()
