c3e06faf3262bb3d7d417aba5b7dc1e13419b4a4
Matt Nedrich <matt.nedrich@atomicobject.com>
2015-03-09 23:11:45 -0400

    initial commit

---
+++ gradient_descent_example.py
@@ -0,0 +1,49 @@
+from numpy import *
+import matplotlib.pyplot as plt
+
+# y = mx + b
+# m is slope, b is y-intercept
+def computer_error_for_line_given_points(b, m, points):
+    totalError = 0
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        totalError += (y - (m * x + b)) ** 2
+    return totalError / float(len(points))
+
+def stepGradient(b_current, m_current, points, learningRate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    new_b = b_current - (learningRate * b_gradient)
+    new_m = m_current - (learningRate * m_gradient)
+    return [new_b, new_m]
+
+def gradient_descent_runner(points, starting_b, starting_m, num_iterations):
+    learning_rate = 0.0001
+    b = starting_b
+    m = starting_m
+    for i in range(num_iterations):
+        b, m = stepGradient(b, m, array(points), learning_rate)
+    return [b, m]
+
+def run():
+    points = genfromtxt("data.csv", delimiter=",")
+    initial_b = 0 # initial y-intercept guess
+    initial_m = 0 # initial slope guess
+    num_iterations = 1000
+    initial_error = computer_error_for_line_given_points(initial_b, initial_m, points)
+    print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, initial_error)
+    print "Running..."
+    [b, m] = gradient_descent_runner(points, initial_b, initial_m, num_iterations)
+    
+    after_error = computer_error_for_line_given_points(b, m, points)
+    print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, after_error)
+
+if __name__ == '__main__':
+    run()
