4e270db50eaff90e7f43dcc33048e1989a054ca8
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-12-06 02:39:40 +0530

    heavily simplified

--- app/simplified.py
+++ app/simplified.py
@@ -1,36 +1,46 @@
-from numpy import genfromtxt, mean
+from matplotlib.pyplot import legend, plot, scatter, show
+from numpy import genfromtxt, mean, sqrt, square
 
 data_set = genfromtxt("data.csv", delimiter=",")
+
 x_list = data_set[:, 0]
 y_list = data_set[:, 1]
 
 intercept = 0
 slope = 0
-predicted_y = 1
 
 learning_rate = 0.0001
+iteration_cycles = 1000
+
 count = len(x_list)
 
-while True:
-    new_y = slope * x_list + intercept
+line_equation = lambda x: slope * x + intercept
 
-    if all(predicted_y == new_y):
-        break
-
-    predicted_y = new_y
-    y_difference = y_list - predicted_y
-
+for _ in range(1000):
+    
+    new_y = list(map(line_equation, x_list))
+    
+    y_difference = y_list - new_y
+    
     intercept_gradient = sum(2 / count * y_difference)
     slope_gradient = sum(2 / count * y_difference * x_list)
-
+    
     intercept += learning_rate * intercept_gradient
     slope += learning_rate * slope_gradient
 
-predicted_y = slope * x_list + intercept
-rmse = mean((y_list - predicted_y) ** 2)
+predicted_y = list(map(line_equation, x_list))
 
-print("60 -", (60 * slope + intercept))
+rmse = sqrt(mean(square(y_list - predicted_y)))
 
 print("intercept =", intercept)
 print("slope =", slope)
 print("rmse =", rmse)
+
+test_x = [44, 32, 63]
+test_y = list(map(line_equation, test_x))
+
+scatter(x_list, y_list, label='original')
+scatter(test_x, test_y, label='prediction')
+plot(x_list, predicted_y, label='line of best fit')
+legend()
+show()
